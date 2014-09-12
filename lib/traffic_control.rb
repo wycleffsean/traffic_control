885a24abf78cb3b666fbd9ee6d7fe9a5eeb9cbe5
Sean Carey <wycleffsean@gmail.com>
2014-09-12 01:46:03 -0400

    added middleware

--- lib/traffic_control.rb
+++ lib/traffic_control.rb
@@ -1,3 +1,4 @@
+require "traffic_control/middleware"
 require "traffic_control/version"
 
 module TrafficControl
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,17 @@
+$: << File.expand_path('../../lib', __FILE__)
+
 require 'coveralls'
 Coveralls.wear!
 
 require 'minitest/autorun'
+require 'traffic_control'
 
 # run all tests if spec_helper is executed directly
 if __FILE__ == $0
   $LOAD_PATH.unshift('lib', 'spec')
   Dir.glob('./spec/**/*_spec.rb') { |f| require f }
+end
+
+def context(*args, &block)
+  describe(*args, &block)
 end
